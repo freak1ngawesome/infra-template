c455dc57b08a3a83acbc3a67d21a5220edd2c3fa
KremeshevD <56875179+KremeshevD@users.noreply.github.com>
2022-10-27 11:37:09 +0300

    test 1

---
+++ .github/postReleaseMessage.js
@@ -0,0 +1,47 @@
+const axios = require("axios");
+
+const postReleaseMessage = async () => {
+  let github = process.env.github;
+  let curTag = github.ref_name;
+  let prevTag = null;
+  const repositoryLink =
+    "https://api.github.com/repos/KremeshevD/infra-template";
+  const headersGit = {
+    headers: { Authorization: github.secrets.GITHUBAPI_TOKEN },
+  };
+  const headersTreker = {
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+      Authorization: `OAuth ${github.secrets.YANDEX_AUTH_TOKEN}`,
+      "X-Org-ID": github.secrets.APP_ID,
+    },
+  };
+
+  try {
+    prevTag = await axios.get(repositoryLink + "/releases/latest", headersGit);
+
+    if (!prevTag) {
+      responce = await axios.get(repositoryLink + "/commits", headersGit);
+      responce = responce.data;
+    } else {
+      responce = await axios.get(
+        `https://api.github.com/repos/nyamzmeya/infra-template/compare/${prevTag.data.tag_name}...${curTag}`,
+        headersGit
+      );
+      responce = responce.data.commits;
+    }
+    let result = "";
+    responce.forEach((commit) => {
+      result += `${commit.sha} ${commit.commit.author.name} ${commit.commit.message} `;
+    });
+
+    await axios.post(
+      "https://api.tracker.yandex.net/v2/issues/HOMEWORKSHRI-135/comments",
+      {
+        text: result,
+      },
+      headersTreker
+    );
+  } catch (error) {}
+};
+postReleaseMessage().then();
